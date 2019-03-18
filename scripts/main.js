82bee2d947d61872a4cdc28f746cfbf5deffe0e3
Surik4t <48680532+Surik4t@users.noreply.github.com>
2019-03-18 20:06:46 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+var myHeading = document.querySelector("h1");
+var myImage = document.querySelector("img");
+var myVid = document.getElementById("myVid");
+
+myImage.onclick = function() {
+		var mySrc = myImage.getAttribute("src");
+		if(mySrc === "images/butt.jpg") {
+				myImage.style.display = "none";
+				myHeading.textContent = "v v v v v v v v v v v v v v v v";
+				var x = document.querySelector('a').textContent = "Warned ya..... ";
+				myVid.style.display = "block";
+				myVid.play();
+		}
+}
+
+var myButton = document.querySelector("button");
+
+
+function setUserName() {
+	var myName = prompt("What's your name?");
+	localStorage.setItem("name",myName);
+	myHeading.innerHTML = myHeading.textContent + ", " + myName;
+}
+
+if(!localStorage.getItem("name")) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem("name");
+	myHeading.innerHTML = myHeading.textContent + ", " + storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
